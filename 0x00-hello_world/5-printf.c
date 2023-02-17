6fd5481b9d51ede6bbf5b00b7c3c01d1bf3ed0fd
diaragom <wanlewislegend@gmail.com>
2023-02-16 17:52:41 -0800

    trailing space headache

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - prints a predetermined phrase followed by a newline 
  * Return: 0 
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	trim(s)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
+
